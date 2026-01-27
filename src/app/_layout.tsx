69183202b49c6243376ef50e62cfcaf3e1b63640
Krystof Woldrich <krystofwoldrich@gmail.com>
2026-01-27 17:31:34 +0100

    Upgrade to Expo SDK 55 Preview 7

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,78 +1,88 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Tabs as WebTabs } from "expo-router/tabs";
-import {
-  Icon,
-  Label,
-  NativeTabs,
-  VectorIcon,
-} from "expo-router/unstable-native-tabs";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 import { Platform, useWindowDimensions } from "react-native";
 
 export default function Layout() {
-  const { width } = useWindowDimensions();
+  return (
+    <ThemeProvider>
+      <TabsLayout />
+    </ThemeProvider>
+  );
+}
 
+function TabsLayout() {
+  if (process.env.EXPO_OS === "web") {
+    return <WebTabsLayout />;
+  } else {
+    return <NativeTabsLayout />;
+  }
+}
+
+function WebTabsLayout() {
+  const { width } = useWindowDimensions();
   const isMd = width >= 768;
   const isLg = width >= 1024;
 
   return (
-    <ThemeProvider>
-      {process.env.EXPO_OS === "web" ? (
-        <WebTabs
-          screenOptions={{
-            headerShown: false,
-            ...(isMd
-              ? {
-                  tabBarPosition: "left",
-                  tabBarVariant: "material",
-                  tabBarLabelPosition: isLg ? undefined : "below-icon",
-                }
-              : {
-                  tabBarPosition: "bottom",
-                }),
-          }}
-        >
-          <WebTabs.Screen
-            name="(index)"
-            options={{
-              title: "Home",
-              tabBarIcon: (props) => <MaterialIcons {...props} name="home" />,
-            }}
-          />
-          <WebTabs.Screen
-            name="(info)"
-            options={{
-              title: "Info",
-              tabBarIcon: (props) => <MaterialIcons {...props} name="info" />,
-            }}
-          />
-        </WebTabs>
-      ) : (
-        <NativeTabs>
-          <NativeTabs.Trigger name="(index)">
-            <Label>Home</Label>
-            <Icon
-              {...Platform.select({
-                ios: { sf: { default: "house", selected: "house.fill" } },
-                default: {
-                  src: <VectorIcon family={MaterialIcons} name="home" />,
-                },
-              })}
-            />
-          </NativeTabs.Trigger>
-          <NativeTabs.Trigger name="(info)">
-            <Label>Info</Label>
-            <Icon
-              {...Platform.select({
-                ios: { sf: "cursorarrow.rays" },
-                default: {
-                  src: <VectorIcon family={MaterialIcons} name="info" />,
-                },
-              })}
-            />
-          </NativeTabs.Trigger>
-        </NativeTabs>
-      )}
-    </ThemeProvider>
+    <WebTabs
+      screenOptions={{
+        headerShown: false,
+        ...(isMd
+          ? {
+              tabBarPosition: "left",
+              tabBarVariant: "material",
+              tabBarLabelPosition: isLg ? undefined : "below-icon",
+            }
+          : {
+              tabBarPosition: "bottom",
+            }),
+      }}
+    >
+      <WebTabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: (props) => <MaterialIcons {...props} name="home" />,
+        }}
+      />
+      <WebTabs.Screen
+        name="info"
+        options={{
+          title: "Info",
+          tabBarIcon: (props) => <MaterialIcons {...props} name="info" />,
+        }}
+      />
+    </WebTabs>
+  );
+}
+
+function NativeTabsLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          {...Platform.select({
+            ios: { sf: { default: "house", selected: "house.fill" } },
+            default: {
+              src: <NativeTabs.Trigger.VectorIcon family={MaterialIcons} name="home" />,
+            },
+          })}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="info">
+        <NativeTabs.Trigger.Label>Info</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          {...Platform.select({
+            ios: { sf: "cursorarrow.rays" },
+            default: {
+              src: <NativeTabs.Trigger.VectorIcon family={MaterialIcons} name="info" />,
+            },
+          })}
+        />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
